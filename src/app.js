32775107cfb5f006a1c3e4aadb8caaac352e509f
lcfernandez <lcfernandez@cos.ufrj.br>
2022-11-08 22:47:10 -0300

    Instalação e configuração dos pacotes mongodb e dotenv

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
+import { MongoClient } from "mongodb";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 
 
 // instance of express
-
 const app = express();
 
 
 // configs
-
 app.use(cors());
 app.use(express.json());
+dotenv.config();
+
+
+// database connection
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+	db = mongoClient.db(process.env.MONGO_DB);
+});
 
 
 // global variables
+
+
+// auxiliary functions
 
 
 // GET functions
@@ -22,9 +35,5 @@ app.use(express.json());
 // POST functions
 
 
-// auxiliary functions
-
-
 // starts the server
-
 app.listen(5000, () => console.log(`Server running in port: 5000`));
